f518900bb37e5486f8600a7b932dccf71866f453
Jose Simonelli <jsimonel@redhat.com>
2013-10-27 14:57:06 -0400

    disabled debug mode

--- evmcmd.rb
+++ evmcmd.rb
@@ -9,11 +9,11 @@ require 'readline'
 require 'savon'
 
 Savon.configure do |config|
-#  config.log = false
+  config.log = false
   config.pretty_print_xml = true
-#  config.log_level = :info
-#  config.raise_errors = false
-#  HTTPI.log = false
+  config.log_level = :info
+  config.raise_errors = false
+  HTTPI.log = false
 end
 
 def help
