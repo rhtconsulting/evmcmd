e5bf6791b25e30a0d6fea1d94ed1306706626f7d
jsimonelli <jose@redhat.com>
2014-03-24 13:45:57 -0400

    fixed it to show GUID instead of object ID

--- lib/findvms.rb
+++ lib/findvms.rb
@@ -11,10 +11,9 @@ class FindVms
       puts "Error: The Tag is required."
     else
       response = @client.call(:get_vms_by_tag, message: {tag: "#{tag}"})
-      puts "hash"
       response_hash =  response.to_hash[:get_vms_by_tag_response][:return]
-      output = AddHashToArray(response_hash[:item])
-      output.each { |key| puts "ID: #{key[:id]}\t Name: #{key[:name]}" }
+      output = AddHashToArray(response_hash[:item]) 
+      output.each { |key| puts "GUID: #{key[:guid]}\t Name: #{key[:name]}" }
     end
   end
 end
